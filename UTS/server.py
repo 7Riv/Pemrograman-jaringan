88bacc2d773a9be560f40b32507f08b4adece677
Riv <129931439+7Riv@users.noreply.github.com>
2024-04-27 09:08:50 +0700

    Update server.py

--- UTS/server.py
+++ UTS/server.py
@@ -1,38 +1,35 @@
 import socket
 import random
-import threading
 import time
+import threading
 
 def generate_random_color():
     colors = ['red', 'purple', 'white', 'blue', 'green', 'black']
     return random.choice(colors)
 
-def handle_client(client_socket, client_address):
+def handle_client(server_socket, client_address):
     num_questions = 10
     for _ in range(num_questions):
         color = generate_random_color()
         print(f"Sending color {color} to {client_address}")
-        client_socket.send(color.encode())
+        server_socket.sendto(color.encode(), client_address)
         time.sleep(10)
     print(f"Finished sending questions to {client_address}")
-    client_socket.close()
 
 def main():
     server_ip = "127.0.0.1"
     server_port = 12345
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((server_ip, server_port))
-    server_socket.listen(5)
 
     print("Server is running...")
 
     while True:
-        client_socket, client_address = server_socket.accept()
+        data, client_address = server_socket.recvfrom(1024)
         print(f"Connection from {client_address}")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread = threading.Thread(target=handle_client, args=(server_socket, client_address))
         client_thread.start()
-        time.sleep(1)  
 
 if __name__ == "__main__":
     main()
